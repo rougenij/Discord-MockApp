997ed4004a32f701a24a23d8fd1b326b45947e3e
Rouge Nij <rougenij@gmail.com>
2022-02-07 18:19:24 +0200

    only left is socket.io

--- client/src/components/Pages/MainChat/Chat.jsx
+++ client/src/components/Pages/MainChat/Chat.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./chat.css";
 
 function Chat() {
-  return <div>Hi from main Chat page</div>;
+  return (
+    <div className="chat-MainWrapper">
+      <div className="chatMenu">Menu</div>
+      <div className="chatBox">Box</div>
+      <div className="chatOnline">online</div>
+    </div>
+  );
 }
 
 export default Chat;
