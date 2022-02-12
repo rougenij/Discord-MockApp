a800969ff540715b677231dcc433237657d9b948
Rouge Nij <rougenij@gmail.com>
2022-02-12 20:23:59 +0200

    Start a new convo button added

--- client/src/components/Pages/StartNewConvo/StartNewConvo.jsx
+++ client/src/components/Pages/StartNewConvo/StartNewConvo.jsx
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from "react";
 import myApi from "../../../API/api";
+import { useHistory, Link } from "react-router-dom";
 
 function StartNewConvo() {
   const [users, setAllUsers] = useState([]);
+  const [loggedUser, setLoggedUser] = useState("");
+  const [otherAccount, setOtherAccount] = useState("");
+
+  const history = useHistory();
 
   useEffect(() => {
     const fetchAllUsers = async () => {
       const { data } = await myApi.get("/users");
       setAllUsers(data);
+      setLoggedUser(localStorage.getItem("LoggedUserID"));
     };
     fetchAllUsers();
   }, []);
+
+  const handleStartNewConvo = async (e) => {
+    setOtherAccount(e.target.id);
+    if (otherAccount) {
+      myApi
+        .post("/conversation", {
+          senderId: `${loggedUser}`,
+          receiverId: `${otherAccount}`,
+        })
+        .then((data) => history.push("/chat"));
+    }
+  };
 
   const displayAllUsers = () => {
     return users.map((user, i) => {
       if (localStorage.getItem("LoggedUserID") !== user._id) {
         return (
-          <div key={user._id} className="conversation">
+          <div
+            key={user._id}
+            id={user._id}
+            className="conversation"
+            onClick={(e) => {
+              handleStartNewConvo(e);
+            }}
+          >
             <img
               src={user.profileImg}
               alt="profileImg"
@@ -31,7 +56,17 @@ function StartNewConvo() {
     });
   };
 
-  return <div>{users && displayAllUsers()}</div>;
+  return (
+    <div>
+      <h1>
+        Chat feeling lonely?? Double click on a chat to start a new conversation
+      </h1>
+      <Link to="/chat">
+        <button>Return to Chat Page</button>
+      </Link>
+      {users && displayAllUsers()}
+    </div>
+  );
 }
 
 export default StartNewConvo;
